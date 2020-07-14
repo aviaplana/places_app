f68ff99f64aa46c4628e6f3fd94af20adbc689d2
Albert Viaplana <pichi.pride@gmail.com>
2020-07-15 00:43:25 +0200

    Create koin modules for the infrastructure module.

---
+++ infrastructure/src/main/java/com/albertviaplana/chamaassignment/infrastructure/di/NetworkDI.kt
@@ -0,0 +1,42 @@
+package com.albertviaplana.chamaassignment.infrastructure.di
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+        factory { provideGsonConverter() }
+    }
+
+fun provideRetrofit(okHttpClient: OkHttpClient, gsonConverterFactory: GsonConverterFactory, apiUrl: String): Retrofit =
+    Retrofit
+        .Builder()
+        .baseUrl(apiUrl)
+        .client(okHttpClient)
+        .addConverterFactory(gsonConverterFactory)
+        .build()
+
+fun provideGsonConverter(): GsonConverterFactory {
+    val gson = GsonBuilder().apply {
+        setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+    }.create()
+
+    return GsonConverterFactory.create(gson)
+}
+
+fun provideOkHttpClient(authInterceptor: Interceptor) =
+    OkHttpClient()
+        .newBuilder()
+        .addInterceptor(authInterceptor)
+        .addInterceptor(getLoggingInterceptor())
+        .build()
+
+fun getLoggingInterceptor() =
+    HttpLoggingInterceptor().apply {
+        setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
---
+++ infrastructure/src/main/java/com/albertviaplana/chamaassignment/infrastructure/placesRepository/di/PlacesRepositoryDI.kt
@@ -0,0 +1,25 @@
+package com.albertviaplana.chamaassignment.infrastructure.placesRepository.di
+
+import com.albertviaplana.chamaassignment.infrastructure.BuildConfig
+import com.albertviaplana.chamaassignment.infrastructure.di.provideOkHttpClient
+import com.albertviaplana.chamaassignment.infrastructure.di.provideRetrofit
+import com.albertviaplana.chamaassignment.infrastructure.placesRepository.api.PlacesAuthInterceptor
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+
+val PlacesApiModule = module {
+    factory { PlacesAuthInterceptor(BuildConfig.PLACES_API_KEY) }
+
+    factory(named("OkHttpWithPlacesAuth")) {
+        provideOkHttpClient(
+            get(named("PlacesAuthInterceptor"))
+        )
+    }
+
+    single(named("PlacesRetrofit")) {
+        provideRetrofit(
+            get(named("OkHttpWithPlacesAuth")),
+            get(),
+            BuildConfig.PLACES_API_URL)
+    }
+}
